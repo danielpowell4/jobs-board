0ad95dc01b63a868df34b0a1fdb81cd4cad3bbfa
danielpowell4 <danielpowell12@msn.com>
2016-01-18 15:55:17 -0700

    basic new belgium functionality

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,8 @@ function loadData() {
 
     // DOM elements
     var $searchToken = $("#position-token").val();
-    var $positionResults = $("#result-positions");
+
+
 
     // Check value from form is coming in well
     console.log($searchToken);
@@ -19,6 +20,8 @@ loadData;
 
 $('#form-container').submit(loadData);
 
+var $positionResults = $("#result-positions");
+$positionResults.text("");
 
 var indeedURL = 'http://api.indeed.com/ads/apisearch?publisher=3980356173222029&as_and=&as_phr=&as_any=&as_not=&as_ttl=&as_cmp=New+Belgium&jt=all&st=&salary=&radius=25&l=&fromage=any&limit=10&sort=&psf=advsrch&q=company%3A%28New+Belgium%29&l=denver%2C+co&sort=&radius=&st=&jt=&start=&limit=100&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2&format=json'
 
@@ -34,7 +37,23 @@ $.ajax({
     dataType: "jsonp",
     jsonp: "callback",
     success: function( response ) {
-      console.log(response);
+
+      var jobList = response.results; // job list is an array of objects from indeed
+
+      console.log(jobList);
+
+      var post, postTitle, postCompany, postSnippet, postLocation, postDate, postURL;
+      for (var i = 0; i<jobList.length; i++){
+        post = jobList[i];
+        postTitle = '<h2>' + post.jobtitle + '</h2>';
+        postCompany = '<h4>' + post.company + '</h4>';
+        postSnippet = '<p>' + post.snippet + '</p>';
+        postLocation = '<p>' + post.formattedLocationFull + '</p>';
+        postDate = '<p>' + post.date + '</p>';
+        postURL = '<a href="' + post.url + '"> View Posting Here </a>';
+
+        $positionResults.append('<div class="positing-item">' + postTitle + postCompany + postSnippet + postLocation + postDate + postURL + '</div>');
+        }
 
       //  var articleList = response[1];
 
