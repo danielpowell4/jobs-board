198b21ddc014886d86e466a41ebfa515d976c8f8
danielpowell4 <danielpowell12@msn.com>
2016-01-25 13:12:20 -0700

    basic check for all terms in filter

--- scripts/main.js
+++ scripts/main.js
@@ -1,92 +1,12 @@
+/**-----------------------------------------------------------------------------
+  *
+  * Globally needed indexes, DOM element selectors and such
+  *
+  *-----------------------------------------------------------------------------*/
 
+// BASIC JOB LISTS
 var masterList = [];
 var currentList = [];
-
-function filterData() {
-
-    // get searchToken from DOM
-    var $searchToken = $("#position-token").val();
-    // Check value from form is coming in well
-    //console.log($searchToken);
-
-    var $searchTokenArray = $('#position-token').val().split(" "); //splits your keywords at the spaces
-
-    //console.log($searchTokenArray);
-
-    // empty currentList
-    currentList = [];
-
-    //loop over keywords searching for exact match adding matches to currentList
-      for(var i = 0; i < $searchTokenArray.length; i++) {
-        searchThroughJson($searchTokenArray, jobList);
-    };
-
-    // clear position positings
-    $positionResults.text("");
-    console.log("cleared board");
-
-    // post currentList to the board
-    if (currentList.length != 0) {
-      for (var i = 0; i<currentList.length; i++) {
-        var post = currentList[i];
-        postTitle = '<h2>' + post.jobtitle + '</h2>';
-        postCompany = '<h4>' + post.company + '</h4>';
-        postSnippet = '<p>' + post.snippet + '</p>';
-        postLocation = '<p>' + post.formattedLocationFull + '</p>';
-        postDate = '<p>' + post.date + '</p>';
-        postURL = '<a href="' + post.url + '"> View Posting Here </a>';
-
-        $positionResults.append('<div class="positing-item">' + postTitle + postCompany + postSnippet + postLocation + postDate + postURL + '</div>');
-        }
-      }  else {
-      $positionResults.text("No results to display. Try something else. There's cool job out there for you!");
-    }
-
-return false;
-};
-
-function nameMatch(searchTerm) {
-  return searchTerm.length > 2;
-}
-
-function searchThroughJson(needle, JSONArray) {
-//  console.log('searchThroughJson RAN');
-  needle = needle.toLowerCase();
-  for (var i = 0; i < JSONArray.length; i++) {
-  //  console.log("inside of the for loop");
-  //  console.log(JSONArray[i]);
-  // console.log(i+1);
-    if (JSONArray[i].jobtitle.toLowerCase().search(needle) != -1) { //check title for match
-      currentList.push(JSONArray[i]);
-    } else if (JSONArray[i].snippet.toLowerCase().search(needle) != -1) { // check snippet -- requires exact match
-      currentList.push(JSONArray[i]);
-    } else if (JSONArray[i].company.toLowerCase().search(needle) != -1) { // check company -- requires exact match
-      currentList.push(JSONArray[i]);
-    } else if (JSONArray[i].state == needle) { // check state -- requires exact match
-      currentList.push(JSONArray[i]);
-    }  else {
-      console.log('nothing found');
-      }
-
-    }
-
-
-};
-
-/**
-  *
-  * clear out old data before new request
-  *  $positionResults.text("");
-  *
-  */
-
-$('#form-container').submit(filterData);
-
-var $positionResults = $("#result-positions");
-$positionResults.text(""); // clears the positionResults... built to be called after a search button is pressed
-
-var masterList = [];
-
 var jobList = [
 
                 {
@@ -365,69 +285,139 @@ var jobList = [
 
     ]; // job list is an array of objects from indeed
 
-var currentList = [];
+
+//DOM ELEMENTS FOR COUNTER
+var $companyCount = $('#companyCount');
+var $jobCount = $('#jobCount');
+
+
+/**-----------------------------------------------------------------------------
+  *
+  * Basic functions for clearing the board and filtering results
+  *
+  *-----------------------------------------------------------------------------*/
+
+  $('#form-container').submit(filterData);
+  var $positionResults = $("#result-positions");
+
+  function clearBoard() {
+    $positionResults.text("");
+  };
+
+  function filterData() {
+
+      // get searchToken from DOM
+      var $searchToken = $("#position-token").val().toLowerCase();
+      // Check value from form is coming in well
+      //console.log($searchToken);
+
+      var $searchTokenArray = $('#position-token').val().toLowerCase().split(" "); //splits your keywords at the spaces
+
+      //console.log($searchTokenArray);
+
+      // empty currentList
+      currentList = [];
+
+      // loop over keywords searching for exact match adding matches to currentList
+        for(var i = 0; i < $searchTokenArray.length; i++) {
+          //console.log($searchTokenArray[i]);
+          searchThroughJson($searchTokenArray[i], jobList);  // inclusive way to do it... "any match displays results"
+      };
+      // go through the list again and clear out things that don't match all search terms
+        for(var i=0; i<$searchTokenArray.length; i++){
+            ensureAllMatch($searchTokenArray[i], currentList); // clears out any 'maybes'
+        };
+
+      // clear position positings
+      clearBoard();
+        //console.log("cleared board");
+
+      // post currentList to the board
+      if (currentList.length != 0) {
+        for (var i = 0; i<currentList.length; i++) {
+          var post = currentList[i];
+          postTitle = '<h2>' + post.jobtitle + '</h2>';
+          postCompany = '<h4>' + post.company + '</h4>';
+          postSnippet = '<p>' + post.snippet + '</p>';
+          postLocation = '<p>' + post.formattedLocationFull + '</p>';
+          postDate = '<p>' + post.date + '</p>';
+          postURL = '<a href="' + post.url + '"> View Posting Here </a>';
+          $positionResults.append('<div class="positing-item">' + postTitle + postCompany + postSnippet + postLocation + postDate + postURL + '</div>');
+          }
+        }  else {
+        $positionResults.text("No results to display. Try something else. There's cool job out there for you!");
+      }
+
+  return false;
+  };
+
+
+  function searchThroughJson(needle, JSONArray) {
+
+    /**
+      * First pass through the job list that fills the currentList
+      * with all potentials that meet one of the terms
+      **/
+
+    for (var i = 0; i < JSONArray.length; i++) {
+
+      if (! currentList.includes(JSONArray[i]) ) { // check to see if array item has already been added
+
+          if (JSONArray[i].jobtitle.toLowerCase().search(needle) != -1) { //check title for match
+            currentList.push(JSONArray[i]);
+          } else if (JSONArray[i].snippet.toLowerCase().search(needle) != -1) { // check snippet
+            currentList.push(JSONArray[i]);
+          } else if (JSONArray[i].company.toLowerCase().search(needle) != -1) { // check company
+            currentList.push(JSONArray[i]);
+          } else if (JSONArray[i].state == needle) { // check state -- requires exact match
+            currentList.push(JSONArray[i]);
+          }  else {
+          //  console.log('nothing found');
+            }
+      }
+
+    };
+
+  };
+
+
+  function ensureAllMatch(needle, potentialList) {
+
+    /**
+      * Second Pass that pops out objects whose pieces don't match all terms
+      * of the current search
+      **/
+
+          for (var i = 0; i < potentialList.length; i++) {
+            if (  potentialList[i].jobtitle.toLowerCase().search(needle) == -1 &&
+                  potentialList[i].snippet.toLowerCase().search(needle) == -1 &&
+                  potentialList[i].company.toLowerCase().search(needle) == -1  ) {
+                    potentialList.splice(i, 1);
+                    console.log(currentList);
+            } else {
+                  //  console.log("That's a pretty number");
+            };
+
+          };
+  };
+
+
+/**-----------------------------------------------------------------------------
+  *
+  * Extract the basics and put the inital list to the board/masterList
+  *
+  *-----------------------------------------------------------------------------*/
 
 var post, postTitle, postCompany, postSnippet, postLocation, postDate, postURL;
 
 function extractInfo(position) {
-  post = postition;
-  //  var xyz = new Post(); <-- need something like that going on
-  this.prototype.postTitle = '<h2>' + post.jobtitle + '</h2>';
-  this.prototype.postCompany = '<h4>' + post.company + '</h4>';
-  this.prototype.postSnippet = '<p>' + post.snippet + '</p>';
-  this.prototype.postLocation = '<p>' + post.formattedLocationFull + '</p>';
-  this.prototype.postDate = '<p>' + post.date + '</p>';
-  this.prototype.postURL = '<a href="' + post.url + '"> View Posting Here </a>';
-  console.log(this.prototype);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function Post() {
-  this.currentList = [];
-};
-
-//Post.prototype.currentList = [];
-/*
-Post.prototype.addToCurrentList = function(newPost) {
-  this.currentList.push(newPost);
-};
-
-Post.prototype.clearBoard = function() {
-  $positionResults.text("");
-};
-
-Post.prototype.pinToBoard = function(list) {
-  var length = list.length;
-
-  for (var i=0; i<length; i++) {
-    $positionResults.append('<div class="positing-item">' + list[i].postTitle + list[i].postCompany + list[i].postSnippet + list[i].postLocation + list[i].postDate + list[i].postURL + '</div>')
-  }
-
-};
-*/
-function extractInfo(position) {
-  post = position;
-  that.postTitle = '<h2>' + post.jobtitle + '</h2>';
-  that.postCompany = '<h4>' + post.company + '</h4>';
-  that.postSnippet = '<p>' + post.snippet + '</p>';
-  that.postLocation = '<p>' + post.formattedLocationFull + '</p>';
-  that.postDate = '<p>' + post.date + '</p>';
-  that.postURL = '<a href="' + post.url + '"> View Posting Here </a>';
+    post = position;
+    that.postTitle = '<h2>' + post.jobtitle + '</h2>';
+    that.postCompany = '<h4>' + post.company + '</h4>';
+    that.postSnippet = '<p>' + post.snippet + '</p>';
+    that.postLocation = '<p>' + post.formattedLocationFull + '</p>';
+    that.postDate = '<p>' + post.date + '</p>';
+    that.postURL = '<a href="' + post.url + '"> View Posting Here </a>';
 
 }
 
@@ -446,39 +436,7 @@ for (var i = 0; i<jobList.length; i++){
   pinToBoard(jobList[i]);
 }
 
-/*
-for (var i = 0; i<jobList.length; i++){
-  post = jobList[i];
-  postTitle = '<h2>' + post.jobtitle + '</h2>';
-  postCompany = '<h4>' + post.company + '</h4>';
-  postSnippet = '<p>' + post.snippet + '</p>';
-  postLocation = '<p>' + post.formattedLocationFull + '</p>';
-  postDate = '<p>' + post.date + '</p>';
-  postURL = '<a href="' + post.url + '"> View Posting Here </a>';
 
-  masterList.push(post); // add to masterList
-
-  $positionResults.append('<div class="positing-item">' + postTitle + postCompany + postSnippet + postLocation + postDate + postURL + '</div>');
-  }
-*/
-
-
-
-
-
-
-
-
-
-
-
-/* -----------------------------------------------------------------------------------------------------
- *        Counter "App"
- * ----------------------------------------------------------------------------------------------------- */
-
-//DOM ELEMENTS FOR COUNTER
-var $companyCount = $('#companyCount');
-var $jobCount = $('#jobCount');
 
 
 
@@ -532,7 +490,7 @@ var indeedURL = 'http://api.indeed.com/ads/apisearch?publisher=3980356173222029&
 
       /* --------------------------------------------------------------------
              loop through the results, push them to masterList and place them on the job board
-         -------------------------------------------------------------------- * / <------------------ GET RID OF THIS SPACE
+         -------------------------------------------------------------------- * / <------------------ TODO GET RID OF THIS SPACE
 
             var post, postTitle, postCompany, postSnippet, postLocation, postDate, postURL;
 
@@ -566,7 +524,7 @@ var indeedURL = 'http://api.indeed.com/ads/apisearch?publisher=3980356173222029&
   //console.log('Numver of Companies Queried: ' + companySearchList.length);
 
 
------------------------------------------------------------------------------------------------------- Dont forget to get rid of the space from above */
+------------------------------------------------------------------------------------------------------ TODO Dont forget to get rid of the space from above */
 
 
 
